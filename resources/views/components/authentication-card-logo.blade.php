558865450b4e7915b94ba57c3e378ededdc35041
antoniosanchezwebdev <128600888+antoniosanchezwebdev@users.noreply.github.com>
2023-06-13 10:55:56 +0200

    arregliyos

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{asset('img/logo.png')}}" width="25%" height="25%" style="margin-left:50px !important;">
+<img src="{{asset('img/logo.png')}}" width="25%" height="25%" style="margin-left:360px !important;">
