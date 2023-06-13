e4c09d205209f19639b6a8629e1a281c49680373
antoniosanchezwebdev <128600888+antoniosanchezwebdev@users.noreply.github.com>
2023-06-14 01:07:54 +0200

    pep

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -4,11 +4,10 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-    <script src="https://cdn.tailwindcss.com"></script>
+
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="grid grid-cols-3 gap-4">
-                <!-- Últimas calificaciones de amigos -->
+        <div>
+            <div>
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
                         <h2 class="text-2xl mb-2">Últimas calificaciones</h2>
