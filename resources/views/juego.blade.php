3c1053f734218df03c18321c2821a12cc4bddd11
antoniosanchezwebdev <128600888+antoniosanchezwebdev@users.noreply.github.com>
2023-06-15 13:16:58 +0200

    g

--- resources/views/juego.blade.php
+++ resources/views/juego.blade.php
@@ -31,7 +31,6 @@
                     </div>
                 </div>
             </div>
-            <div id="hola">si</div>
 
             <script>
                 window.onload = function() {
--- resources/views/juegoPersonalizado/play.blade.php
+++ resources/views/juegoPersonalizado/play.blade.php
@@ -7,6 +7,13 @@
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"
         integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
     @livewire('juego-personalizado.play', ['id' => $id])
+
+    <div>
+        <div class="text-xl font-medium text-black">Aviso</div>
+        <p class="text-gray-500">Debido a problemas con el servidor, el juego puede tardar algo en cargar por
+            primera vez.</p>
+    </div>
+
     <div class="py-12">
         <div class="max-w-7x1 mx-auto sm:px-6 lg:px-8">
             <div id="juegoContainer" width="1620px" height="720px" autofocus="true">
@@ -14,4 +21,18 @@
                 </div>
             </div>
         </div>
+
+        <script>
+            window.onload = function() {
+                var gameElement = document.getElementById('juegoContainer'); // Cambia 'miJuego' al ID de tu contenedor de Phaser
+                var gamePosition = gameElement.getBoundingClientRect().top;
+                var offsetPosition = gamePosition - window.innerHeight / 2 + gameElement.clientHeight / 2;
+
+                window.scrollTo({
+                    top: offsetPosition,
+                    behavior: "smooth"
+                });
+            };
+        </script>
+
 </x-app-layout>
