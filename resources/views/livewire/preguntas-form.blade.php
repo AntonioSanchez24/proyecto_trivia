c2e6786064728e5bf6714ec302f73c78fdc8fc9a
antoniosanchezwebdev <128600888+antoniosanchezwebdev@users.noreply.github.com>
2023-06-15 12:55:24 +0200

    a

--- resources/views/livewire/preguntas-form.blade.php
+++ resources/views/livewire/preguntas-form.blade.php
@@ -1,7 +1,7 @@
 <div>
     <x-app-layout>
         <x-slot name="header">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
                 {{ __('Dashboard') }}
             </h2>
         </x-slot>
@@ -10,21 +10,21 @@
 
         <div class="py-12">
 
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white rounded-lg shadow-md p-4 flex items-start mb-4">
+            <div class="mx-auto  sm:px-6 lg:px-8">
+                <div class="flex items-start p-4 mb-4 bg-white rounded-lg shadow-md">
                     <div class="mr-6">
                         <img src="{{ $paquete->photo_url }}" alt="{{ $paquete->nombre }}"
-                            class="w-20 h-20 rounded-full mr-4"> &nbsp;
+                            class="w-20 h-20 mr-4 rounded-full"> &nbsp;
                     </div>
                     <div>
-                        <h2 class="text-2xl font-bold mb-2">{{ $paquete->nombre }}</h2>
-                        <p class="text-gray-600 mb-4">{{ $paquete->descripcion }}</p>
+                        <h2 class="mb-2 text-2xl font-bold">{{ $paquete->nombre }}</h2>
+                        <p class="mb-4 text-gray-600">{{ $paquete->descripcion }}</p>
                     </div>
                 </div>
                 <div class="grid grid-cols-3 gap-4">
-                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div class="p-6 border border-black border-solid border-2 rounded-lg shadow-lg bg-white">
-                            <h2 class="block text-gray-700 text-lg font-medium mb-2">
+                    <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                        <div class="p-6 bg-white border border-2 border-black border-solid rounded-lg shadow-lg">
+                            <h2 class="block mb-2 text-lg font-medium text-gray-700">
                                 Añade/Modifica una pregunta
                             </h2>
 
@@ -35,7 +35,7 @@
                                 <div>
                                     <x-label for="pregunta" value="{{ __('Pregunta') }}" />
                                     <input id="pregunta"
-                                        class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+                                        class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
                                         type="text" name="pregunta" wire:model="pregunta"
                                         placeholder="¿Qué fue antes..?" />
                                     @error('pregunta')
@@ -46,7 +46,7 @@
                                 <div class="mt-4">
                                     <x-label for="respuesta_correcta" value="{{ __('Respuesta correcta') }}" />
                                     <input id="respuesta_correcta"
-                                        class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+                                        class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
                                         type="text" name="respuesta_correcta" wire:model="respuesta_correcta"
                                         placeholder="¡La gallina!" />
                                     @error('respuesta_correcta')
@@ -56,21 +56,21 @@
                                 <div class="mt-4">
                                     <x-label for="respuesta_incorrecta1" value="{{ __('Respuestas incorrectas') }}" />
                                     <input id="respuesta_incorrecta1"
-                                        class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+                                        class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
                                         type="text" name="respuesta_incorrecta1" wire:model="respuesta_incorrecta1"
                                         placeholder="Eso de ahí." />
                                     @error('respuesta_incorrecta1')
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
                                     <input id="respuesta_incorrecta2"
-                                        class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+                                        class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
                                         type="text" name="respuesta_incorrecta2" wire:model="respuesta_incorrecta2"
                                         placeholder="Pasapalabra." />
                                     @error('respuesta_incorrecta2')
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
                                     <input id="respuesta_incorrecta3"
-                                        class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+                                        class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
                                         type="text" name="respuesta_incorrecta3" wire:model="respuesta_incorrecta3"
                                         placeholder="Un huevo." />
                                     @error('respuesta_incorrecta3')
@@ -81,7 +81,7 @@
                                 <div class="flex items-center justify-start mt-4">
                                     <x-label for="categoria" value="{{ __('Categoría') }}" style="margin-left:20px;" />
                                     <input id="categoria"
-                                        class="mt-1 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+                                        class="mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
                                         wire:model="categoria" type="text" name="categoria" placeholder="Eso de ahí."
                                         style="margin-left:5px;" />
                                     @error('respuesta_correcta')
@@ -90,7 +90,7 @@
                                     <x-label for="dificultad" value="{{ __('Dificultad') }}"
                                         style="margin-left:20px;" />
                                     <input id="dificultad"
-                                        class="mt-1 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+                                        class="mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
                                         wire:model="dificultad" type="number" name="dificultad"
                                         placeholder="Eso de ahí." style="margin-left:5px;" />
                                 </div>
@@ -98,14 +98,14 @@
                                 <div class="flex items-center justify-end mt-4">
                                     <div>
                                         <button type="submit"
-                                            class="px-4 py-2 bg-blue-500 text-white rounded-md">Guardar</button>
+                                            class="px-4 py-2 text-white bg-blue-500 rounded-md">Guardar</button>
                                     </div>
                                 </div>
                             </form>
                         </div>
                         <br>
-                        <div class="p-6 border border-black border-solid border-2 rounded-lg shadow-lg bg-white">
-                            <h2 class="block text-gray-700 text-lg font-medium mb-2">
+                        <div class="p-6 bg-white border border-2 border-black border-solid rounded-lg shadow-lg">
+                            <h2 class="block mb-2 text-lg font-medium text-gray-700">
                                 Preguntas añadidas
                             </h2>
                             <div class="mt-10 space-y-6">
@@ -129,9 +129,9 @@
                                         <p>{{ $pregunta['dificultad'] }}</p>
                                         <div class="mt-4">
                                             <button wire:click="cargarPregunta({{ $pregunta['id'] }})"
-                                                class="px-4 py-2 bg-blue-500 text-white rounded-md">Editar</button>
+                                                class="px-4 py-2 text-white bg-blue-500 rounded-md">Editar</button>
                                             <button wire:click="eliminarPregunta({{ $pregunta['id'] }})"
-                                                class="px-4 py-2 bg-red-500 text-white rounded-md">Eliminar</button>
+                                                class="px-4 py-2 text-white bg-red-500 rounded-md">Eliminar</button>
                                         </div>
                                     </div>
                                 @endforeach
