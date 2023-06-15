6e1b1de4f3948195663756ad3a5b95d8734c7e7c
antoniosanchezwebdev <128600888+antoniosanchezwebdev@users.noreply.github.com>
2023-06-15 12:56:11 +0200

    a

--- resources/views/livewire/preguntas-form.blade.php
+++ resources/views/livewire/preguntas-form.blade.php
@@ -2,16 +2,15 @@
     <x-app-layout>
         <x-slot name="header">
             <h2 class="text-xl font-semibold leading-tight text-gray-800">
-                {{ __('Dashboard') }}
+                {{ __('Editar preguntas') }}
             </h2>
         </x-slot>
 
 
 
         <div class="py-12">
-
-            <div class="mx-auto  sm:px-6 lg:px-8">
-                <div class="flex items-start p-4 mb-4 bg-white rounded-lg shadow-md">
+            <div class="mx-auto sm:px-6 lg:px-8">
+                <div class="flex items-center p-4 mb-4 bg-white rounded-lg shadow-md">
                     <div class="mr-6">
                         <img src="{{ $paquete->photo_url }}" alt="{{ $paquete->nombre }}"
                             class="w-20 h-20 mr-4 rounded-full"> &nbsp;
