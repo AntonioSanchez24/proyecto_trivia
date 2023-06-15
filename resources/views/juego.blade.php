c1e556a27aa24f7abe6c23f303f4631e87dc61e0
antoniosanchezwebdev <128600888+antoniosanchezwebdev@users.noreply.github.com>
2023-06-15 13:16:02 +0200

    ultimo de verdad

--- resources/views/juego.blade.php
+++ resources/views/juego.blade.php
@@ -8,12 +8,41 @@
         integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
     <livewire:juego>
 
+        <br>
+
+        <div class="flex items-center max-w-2xl p-6 mx-auto space-x-4 bg-white shadow-md rounded-xl">
+            <div class="flex-shrink-0">
+                <svg class="w-12 h-12 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor" aria-hidden="true">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
+                </svg>
+            </div>
+            <div>
+                <div class="text-xl font-medium text-black">Aviso</div>
+                <p class="text-gray-500">Debido a problemas con el servidor, el juego puede tardar algo en cargar por
+                    primera vez.</p>
+            </div>
+        </div>
+
         <div class="py-12">
-            <x-banner data="{ show: true, style: danger, message: 'El juego puede tardar un poco en cargar en conexiones limitadas.' }"/>
             <div class="mx-auto max-w-7x1 sm:px-6 lg:px-8">
                 <div id="juegoContainer" width="1620px" height="720px" autofocus="true">
                     <div class="overflow-hidden bg-white shadow-xl sm:rounded-lg">
                     </div>
                 </div>
             </div>
+            <div id="hola">si</div>
+
+            <script>
+                window.onload = function() {
+                    var gameElement = document.getElementById('juegoContainer'); // Cambia 'miJuego' al ID de tu contenedor de Phaser
+                    var gamePosition = gameElement.getBoundingClientRect().top;
+                    var offsetPosition = gamePosition - window.innerHeight / 2 + gameElement.clientHeight / 2;
+
+                    window.scrollTo({
+                        top: offsetPosition,
+                        behavior: "smooth"
+                    });
+                };
+            </script>
 </x-app-layout>
