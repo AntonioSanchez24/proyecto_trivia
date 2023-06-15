ac026ead353caf5e9bb66ab7f01b24771a667b95
antoniosanchezwebdev <128600888+antoniosanchezwebdev@users.noreply.github.com>
2023-06-15 10:05:30 +0200

    pum

--- resources/views/livewire/creador-preguntas/edit.blade.php
+++ resources/views/livewire/creador-preguntas/edit.blade.php
@@ -1,15 +1,15 @@
 <div>
     <x-app-layout>
         <x-slot name="header">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
                 {{ __('Editar paquete de preguntas') }}
             </h2>
         </x-slot>
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="px-4 py-4 overflow-hidden bg-white shadow-xl sm:rounded-lg">
                     @if (session()->has('message'))
-                        <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md my-3"
+                        <div class="px-4 py-3 my-3 text-teal-900 bg-teal-100 border-t-4 border-teal-500 rounded-b shadow-md"
                             role="alert">
                             <div class="flex">
                                 <div>
@@ -20,22 +20,25 @@
                     @endif
                     <div class="w-full max-w-md mx-auto mt-6">
                         <form wire:submit.prevent="actualizarTodo" class="space-y-5">
-                            <div class="p-6 border border-black border-solid border-2 rounded-lg shadow-lg bg-white">
-                                <label class="block text-gray-700 text-lg font-medium mb-2" for="nombre">
+                            <div class="p-6 bg-white border border-2 border-black border-solid rounded-lg shadow-lg">
+                                <label class="block mb-2 text-lg font-medium text-gray-700" for="nombre">
                                     Nombre
                                 </label>
                                 <input wire:model="nombre"
-                                    class="shadow-inner appearance-none border border-grey-200 rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    class="w-full px-3 py-3 leading-tight text-gray-700 border rounded shadow-inner appearance-none border-grey-200 focus:outline-none focus:shadow-outline"
                                     id="nombre" type="text" placeholder="Introduce tu nombre"
                                     value="{{ $paquete->nombre }}">
+                                @error('nombre')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
                             </div>
                             <br>
-                            <div class="p-6 border border-black border-solid border-2 rounded-lg shadow-lg bg-white">
-                                <label class="block text-gray-700 text-lg font-medium mb-2" for="photo">
+                            <div class="p-6 bg-white border border-2 border-black border-solid rounded-lg shadow-lg">
+                                <label class="block mb-2 text-lg font-medium text-gray-700" for="photo">
                                     Foto
                                 </label>
                                 <input wire:model="photo"
-                                    class="shadow-inner appearance-none border border-grey-200 rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    class="w-full px-3 py-3 leading-tight text-gray-700 border rounded shadow-inner appearance-none border-grey-200 focus:outline-none focus:shadow-outline"
                                     id="photo" type="file">
                                 <!-- Vista previa de la imagen -->
                                 @if ($photo)
@@ -45,26 +48,31 @@
                                         <img src="{{ Storage::url($paquete->photo) }}" alt="Foto existente">
                                     @endif
                                 @endif
-                                <!-- Cuando la foto existente está presente -->
+                                @error('photo')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
                             </div>
 
 
                             <br>
-                            <div class="p-6 border border-black border-solid border-2 rounded-lg shadow-lg bg-white">
-                                <label class="block text-gray-700 text-lg font-medium mb-2" for="description">
+                            <div class="p-6 bg-white border border-2 border-black border-solid rounded-lg shadow-lg">
+                                <label class="block mb-2 text-lg font-medium text-gray-700" for="description">
                                     Descripción
                                 </label>
                                 <textarea wire:model="description"
-                                    class="shadow-inner appearance-none border border-grey-200 rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    class="w-full px-3 py-3 leading-tight text-gray-700 border rounded shadow-inner appearance-none border-grey-200 focus:outline-none focus:shadow-outline"
                                     id="description" placeholder="Introduce una descripción"></textarea>
                             </div>
+                            @error('description')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
                             <br>
-                            <div class="p-6 border border-black border-solid border-2 rounded-lg shadow-lg bg-white">
-                                <label class="block text-gray-700 text-lg font-medium mb-2" for="categoria">
+                            <div class="p-6 bg-white border border-2 border-black border-solid rounded-lg shadow-lg">
+                                <label class="block mb-2 text-lg font-medium text-gray-700" for="categoria">
                                     Categoría
                                 </label>
                                 <select wire:model="categoria"
-                                    class="shadow-inner appearance-none border border-grey-200 rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    class="w-full px-3 py-3 leading-tight text-gray-700 border rounded shadow-inner appearance-none border-grey-200 focus:outline-none focus:shadow-outline"
                                     id="categoria">
                                     <option value="">-- Selecciona una categoría --</option>
                                     <option value="categoria1">Ciencia</option>
@@ -72,9 +80,12 @@
                                     <option value="categoria3">Entretenimiento</option>
                                 </select>
                             </div>
+                            @error('categoria')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
                             <div class="flex items-center justify-between">
                                 <button
-                                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded-lg focus:outline-none focus:shadow-outline"
+                                    class="px-4 py-3 font-bold text-white bg-blue-500 rounded-lg hover:bg-blue-700 focus:outline-none focus:shadow-outline"
                                     type="submit">
                                     Actualizar
                                 </button>
